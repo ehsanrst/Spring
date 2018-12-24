303866c3e5089f5442714ba6f0e2ab1f3cd9cc64
Ehsan Roostai <ehsan.roostai@gmail.com>
2018-12-24 15:25:08 +0330

    Clean project

--- src/main/com/Dotin/App.java
+++ src/main/com/Dotin/App.java
@@ -1,22 +1,7 @@
 package com.Dotin;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-/**
- * Bean Definition Inheritance
- */
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
-        objA.getMessage1();
-        objA.getMessage2();
-
-        HelloIran objB = (HelloIran) context.getBean("helloIran");
-        objB.getMessage1();
-        objB.getMessage2(); //will inherit from parent
-        objB.getMessage3();
     }
 }
