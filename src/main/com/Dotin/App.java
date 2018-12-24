083d2622d768d411da1270a457aac8435ad8ada5
Ehsan Roostai <ehsan.roostai@gmail.com>
2018-12-24 14:47:23 +0330

    Bean Definition Inheritance & Bean Definition Template

--- src/main/com/Dotin/App.java
+++ src/main/com/Dotin/App.java
@@ -1,17 +1,22 @@
 package com.Dotin;
 
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Bean Post Processors
+ * Bean Definition Inheritance
  */
 public class App {
     public static void main(String[] args) {
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-        obj.getMessage();
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-        context.registerShutdownHook();
+        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+        objA.getMessage1();
+        objA.getMessage2();
+
+        HelloIran objB = (HelloIran) context.getBean("helloIran");
+        objB.getMessage1();
+        objB.getMessage2(); //will inherit from parent
+        objB.getMessage3();
     }
 }
