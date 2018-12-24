c43f336e02998a390629e7afb135afd033d6adca
Ehsan Roostai <ehsan.roostai@gmail.com>
2018-12-24 12:53:05 +0330

    Bean Life Cycle

--- src/main/com/Dotin/App.java
+++ src/main/com/Dotin/App.java
@@ -1,42 +1,22 @@
 package com.Dotin;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Bean Scopes (singleton)
+ * Bean Life Cycle
+ * there are lists of the activities that take place behind the scene
+ * between the time of bean Instantiation and its destruction
  */
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        obj.getMessage();
 
-        objA.setMessage("I'm Object A");
-        objA.getMessage();
-
-        HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
-        objB.getMessage();
-        //if scope define as prototype result will be null for objB
+        context.registerShutdownHook();
+        /* declared on the AbstractApplicationContext class.
+           This will ensure a graceful shutdown and call the
+           relevant destroy methods so resources are released.*/
     }
 }
-
-/*
-  singleton:
-  This scopes the bean definition to a single instance per Spring
-  IoC container (default).
-  <p>
-  prototype:
-  This scopes a single bean definition to have any number of object instances.
-  <p>
-  request:
-  This scopes a bean definition to an HTTP request. Only valid in the context
-  of a web-aware Spring ApplicationContext.
-  <p>
-  session:
-  This scopes a bean definition to an HTTP session. Only valid in the context
-  of a web-aware Spring ApplicationContext.
-  <p>
-  global-session:
-  This scopes a bean definition to a global HTTP session. Only valid in the context
-  of a web-aware Spring ApplicationContext.
-  */
