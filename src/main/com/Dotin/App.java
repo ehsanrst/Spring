e8d234dc25c7359fbc9818d1860c6adba3e1108f
Ehsan Roostai <ehsan.roostai@gmail.com>
2018-12-26 15:45:03 +0330

    Example for Injecting Bean Dependencies with Java Base Annotation

--- src/main/com/Dotin/App.java
+++ src/main/com/Dotin/App.java
@@ -1,7 +1,22 @@
 package com.Dotin;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Injecting Bean Dependencies
+ */
 public class App {
     public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(TextEditorConfig.class);
 
+        TextEditor textEditor = context.getBean(TextEditor.class);
+        textEditor.spellCheck();
     }
 }
+
+/*
+When @Beans have dependencies on one another, expressing that the dependency
+ is as simple as having one bean method calling another.
+ */
