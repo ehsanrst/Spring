30136d445b79dcdb7c53bc0334b944c2d9e2f4ec
Ehsan Roostai <ehsan.roostai@gmail.com>
2018-12-26 10:50:45 +0330

    Auto Wiring byName

--- src/main/com/Dotin/App.java
+++ src/main/com/Dotin/App.java
@@ -1,58 +1,29 @@
 package com.Dotin;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     public static void main(String[] args) {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        TextEditor textEditor = (TextEditor) context.getBean("textEditor");
+        textEditor.spellCheck();
     }
 }
 
 /*
-The Spring container can autowire relationships between collaborating beans
- without using <constructor-arg> and <property> elements, which helps cut down
-  on the amount of XML configuration you write for a big Spring-based application.
-
 Autowiring Modes:
-    1. no :
-    This is default setting which means no autowiring and you should use
-     explicit bean reference for wiring. You have nothing to do special for
-      this wiring. This is what you already have seen in Dependency Injection
-       chapter.
-
     2. byName :
-    Autowiring by property name. Spring container looks at the properties
+    Autowiring by property_name. Spring container looks at the properties
      of the beans on which autowire attribute is set to byName in the XML
       configuration file. It then tries to match and wire its properties with
        the beans defined by the same names in the configuration file.
+       If matches are found, it will inject those beans. Otherwise, bean(s)
+        will not be wired.
 
-    3. byType :
-    Autowiring by property dataType. Spring container looks at the properties
-     of the beans on which autowire attribute is set to byType in the XML
-      configuration file. It then tries to match and wire a property if its
-       type matches with exactly one of the beans name in configuration file.
-        If more than one such beans exists, a fatal exception is thrown.
-
-    4. constructor :
-    Similar to byType, but type applies to constructor arguments.
-     If there is not exactly one bean of the constructor argument type in the
-      container, a fatal error is raised.
-
-    5. autodetect :
-    Spring first tries to wire using autowire by constructor, if it does not
-     work, Spring tries to autowire by byType.
-
-hint : You can use byType or constructor autowiring mode to wire arrays and other typed-collections.
-        Autowiring works best when it is used consistently across a project.
-
-Limitations :
-    1. Overriding possibility :
-        You can still specify dependencies using <constructor-arg> and <property>
-         settings which will always override autowiring.
-
-    2. Primitive data types :
-        You cannot autowire so-called simple properties such as primitives,
-         Strings, and Classes.
-
-    3. Confusing nature :
-        Autowiring is less exact than explicit wiring, so if possible prefer
-         using explicit wiring.
+        For example,
+         if a bean definition is set to autowire byName in the configuration file,
+          and it contains a spellChecker property (that is, it has a setSpellChecker(...)method),
+           Spring looks for a bean definition named spellChecker, and uses it to
+            set the property.
  */
---
+++ src/main/com/Dotin/TextEditor.java
@@ -0,0 +1,26 @@
+package com.Dotin;
+
+public class TextEditor {
+    private SpellChecker spellChecker;
+    private String name;
+
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
+    }
+
+    public void setSpellChecker(SpellChecker spellChecker) {
+        this.spellChecker = spellChecker;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void spellCheck() {
+        spellChecker.checkSpelling();
+    }
+}
