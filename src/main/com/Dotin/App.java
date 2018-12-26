32d2f263db2c072c5f14ebe5efc5a3aeb188db85
Ehsan Roostai <ehsan.roostai@gmail.com>
2018-12-26 11:56:45 +0330

    Simple Example for @Required Annotation

--- src/main/com/Dotin/App.java
+++ src/main/com/Dotin/App.java
@@ -1,39 +1,24 @@
 package com.Dotin;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Annotation Based Configuration
  */
 public class App {
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
+        Student student = (Student) context.getBean("student");
+        System.out.println("Name : " + student.getName());
+        System.out.println("Age : " + student.getAge());
     }
 }
 /*
-Instead of using XML to describe a bean wiring, you can move the bean configuration
- into the component class itself by using annotations on the relevant class,
-  method, or field declaration.
-
-Annotation injection is performed before XML injection.
- Thus, the latter configuration will override the former for properties wired
-  through both approaches.
-
-Annotation wiring is not turned on in the Spring container by default.
- So, before we can use annotation-based wiring, we will need to enable it
-  in our Spring configuration file.
-
-a few important annotations:
   @Required
     The @Required annotation applies to bean property setter methods.
-
-  @Autowired
-    The @Autowired annotation can apply to bean property setter methods,
-     non-setter methods, constructor and properties.
-
-  @Qualifier
-    The @Qualifier annotation along with @Autowired can be used to remove
-     the confusion by specifying which exact bean will be wired.
-
-  JSR-250 Annotations
-    Spring supports JSR-250 based annotations which include @Resource,
-     @PostConstruct and @PreDestroy annotations.
+     and it indicates that the affected bean property must be populated in
+      XML configuration file at configuration time.
+       Otherwise, the container throws a BeanInitializationException exception.
  */
