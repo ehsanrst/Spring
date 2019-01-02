ce991a06e7610d9275482af423af83901172f676
Ehsan Roostai <ehsan.roostai@gmail.com>
2019-01-02 09:19:47 +0330

    Example for Log4j to logging

--- src/main/com/Dotin/App.java
+++ src/main/com/Dotin/App.java
@@ -1,7 +1,25 @@
 package com.Dotin;
 
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+/*
+Log4j
+ */
 public class App {
-    public static void main(String[] args) {
+    static Logger log = Logger.getLogger(App.class.getName());
 
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        log.info("Going to create HelloSpring Object!");
+        HelloSpring obj = (HelloSpring) context.getBean("helloSpring");
+        obj.getMessage();
+
+        log.info("Exiting the Program.");
     }
 }
+
+/*
+We need create  log4j.properties which defines the standard rules required
+ for Log4J to produce log messages in resource directory.
+ */
---
+++ src/main/com/Dotin/HelloSpring.java
@@ -0,0 +1,13 @@
+package com.Dotin;
+
+public class HelloSpring {
+    private String message;
+
+    public void getMessage() {
+        System.out.println("Your message : " + message);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
